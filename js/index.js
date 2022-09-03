eb3f8908258f633d0a46ce64873b44b419343259
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-09-03 21:20:52 +0600

    modal added

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ const displayNewsByCategoryId = data =>{
     const allCategoryNewsDetails = document.getElementById('news-container');
     allCategoryNewsDetails.textContent=``;
     data.forEach(news => {
-        console.log(news);
+        // console.log(news);
         const {image_url,title,total_view,_id,details,author} = news;
         const {img,name,published_date} = author;
         const newsDiv = document.createElement('div')
@@ -53,21 +53,19 @@ const displayNewsByCategoryId = data =>{
                                     <h3>${published_date}</h3>
                                 </div>
                             </div>
-                            <div>
-                                <span><i class="fa-regular fa-eye"></i></span> 
-                                <span class="font-semibold">${total_view}</span>
+                            <div class="flex items-center >
+                            <span class="font-semibold">View: ${total_view}</span>
                             </div>
                             <div class="flex items-center mt-2.5 mb-5">
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star-half-stroke"></i>
-                                <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">4.5</span>
-                            </div>
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+                        </div>
                             <div>
-                                <button type="button"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">View Details</button>
+                            <label for="my-modal-3" onclick="getModalByNewsId('${_id}')" class="btn btn-primary modal-button">View Details</label>
                             </div>
                         </div>
                  </div>
@@ -78,5 +76,29 @@ const displayNewsByCategoryId = data =>{
     });
 }
 
+const getModalByNewsId = (id) =>{
+    // console.log(_id);
+const url = `https://openapi.programming-hero.com/api/news/${id}`;
+// console.log(url);
+fetch(url)
+.then(res => res.json())
+.then(data => showModal(data.data[0]))
+}
+
+const showModal = data =>{
+// data.forEach(id = )
+const {title,image_url,author,details} = data;
+const {name,published_date} = author;
+console.log(data.category_id)
+const modalBody = document.getElementById('modal-body');
+modalBody.innerHTML=`
+<img src="${image_url}">
+<h3 class="text-lg font-bold">${title}</h3>
+<h3><span class="text-lg text-blue-500 font-bold">Author:</span> ${name ? name : 'Not Found' } </h3>
+<small><span class="font-bold text-blue-500">Date:</span> ${published_date ? published_date: 'N/A'}</small>
+<p class="py-4">${details}</p>
+`
+
+}
 
 loadCategory();
